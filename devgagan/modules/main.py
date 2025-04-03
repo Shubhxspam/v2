4ef88f7ec2a1d2c38fce0d660bc5284317a77aa0
𝐌я.𝐏αѕѕєяву <164321506+Shubhxspam@users.noreply.github.com>
2025-04-03 09:48:03 +0530

    Update main.py

--- devgagan/modules/main.py
+++ devgagan/modules/main.py
@@ -282,7 +282,7 @@ async def batch_link(_, message):
 
         await set_interval(user_id, interval_minutes=300)
         await pin_msg.edit_text(
-            f"Batch completed successfully for {cl} messages 🎉\n\n**__Powered by Team SPY__**",
+            f"Batch completed successfully for {cl} messages 🎉\n\n**__Powered by 𝐌я.𝐏αѕѕєяву__**",
             reply_markup=keyboard
         )
         await app.send_message(message.chat.id, "Batch completed successfully! 🎉")
