ddcfd5a320d267ddce4583cf01c936225799be86
𝐌я.𝐏αѕѕєяву <164321506+Shubhxspam@users.noreply.github.com>
2025-04-03 12:58:59 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -43,10 +43,10 @@ async def subscribe(app, message):
             return 1
       except UserNotParticipant:
         caption = f"Join our channel to use the bot"
-        await message.reply_photo(photo="https://graph.org/file/d44f024a08ded19452152.jpg",caption=caption, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"{url}")]]))
+        await message.reply_photo(photo="https://graph.org/file/119d5fac10c2ac919ca87-8c3a86927f5ce2f63a.jpg",caption=caption, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"{url}")]]))
         return 1
       except Exception:
-         await message.reply_text("Something Went Wrong. Contact us @devgaganin...")
+         await message.reply_text("Something Went Wrong. Contact us @Mrpasserby_1227..")
          return 1
 async def get_seconds(time_string):
     def extract_value_and_unit(ts):
@@ -250,7 +250,7 @@ async def progress_callback(current, total, progress_message):
     f"│ **__Progress:__** {percent:.2f}%\n"
     f"│ **__Uploaded:__** {current_mb:.2f} MB / {total_mb:.2f} MB\n"
     f"╰──────────────────╯\n\n"
-    f"**__Powered by Team SPY__**"
+    f"**__Powered by 𝐌я.𝐏αѕѕєяву__**"
         )
 
         last_update_time = current_time
